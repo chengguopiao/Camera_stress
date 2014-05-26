692665a6387affa4190e98b5c8607424672f3280
chengguopiao <chengguo.piao@borqs.com>
2014-05-26 18:19:14 +0800

    Update stress.py

--- script/stress.py
+++ script/stress.py
@@ -85,6 +85,9 @@ class CameraTest(unittest.TestCase):
         ad.cmd('refresh','/sdcard/DCIM/*')
         if d(resourceId = 'com.android.keyguard:id/glow_pad_view').wait.exists(timeout = 3000):
             d.swipe(366,905,666,905,steps=5) 
+            ad.cmd('adb root')
+            time.sleep(2)
+            ad.cmd('adb remount')
         #Launch social camera
         self._launchCamera()
 
